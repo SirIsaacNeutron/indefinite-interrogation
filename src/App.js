04c92916135a1cf37799b2bdc83076e0b7e0a66f
SirIsaacNeutron <dbadir@cox.net>
2022-06-03 01:22:29 -0700

    Changed App to functional component
    I'm not very good with hooks so this might be temporary. Also I improved
    the button CSS a bit; the mobile website should now look better as
    buttons will be closer to a uniform height.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import MainMenu from './components/MainMenu.js';
+import MainMenu from './components/MainMenu';
 
 import backgroundMusicFile from "./sounds/music.mp3";
 
 import './App.css';
 
-/*function App() {
+function App() {
+  let [gameStarted, setGameStarted] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
-
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <MainMenu />
-
-        <audio className="bg-music" src={backgroundMusicFile} controls autoPlay loop></audio>
-      </>);
+    <>
+      <MainMenu setGameStarted={setGameStarted}/>
+      
+      <audio className="bg-music" src={backgroundMusicFile} controls autoPlay loop></audio>
+    </>
+    );
   }
-}
-
-export default App;
+  
+  export default App;
+  
---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+function MainMenu({ setGameStarted }) {
+
+    return (
+        <>
+            <h1 className="game-header">Indefinite</h1>
+            <p className="menu-question">Ready to begin the interrogation?</p>
+
+            <div className="container">
+                <div className="row row-eq-height">
+                    <div className="col">
+                        <div className="btn button" 
+                        onClick={() => setGameStarted(true)}>Yes. Let's start. (Play)</div>
+                    </div>
+                    <div className="col">
+                        <button className="btn button" data-bs-toggle="modal" data-bs-target="#helpWindow">Where
+                            am I? (Help)</button>
+                    </div>
+                </div>
+                <div className="row row-eq-height">
+                    <div className="col">
+                        <a className="btn button code-link" href="https://github.com/SirIsaacNeutron/indefinite-interrogation" target="_blank" rel="noreferrer">What are you? (Source Code)</a>
+                    </div>
+                    <div className="col">
+                        <div className="btn button" data-bs-toggle="modal" data-bs-target="#musicHelpWindow">What's below the buttons?</div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Help Modal */}
+            <div className="modal fade" id="helpWindow" tabIndex="-1" aria-labelledby="helpWindowLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-body">
+                            <p>As you know, The Incident was a catastrophic global event
+                                in which all world leaders were killed. You are a person of interest brought
+                                to us for questioning.
+                            </p>
+                            We expect you to:
+                            <ol>
+                                <li>Answer questions quickly.</li>
+                                <li>Give identical responses to identical questions.</li>
+                                <li>Accept your fate when it is over.</li>
+                            </ol>
+                            <button type="button" className="btn button" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Music Help Window */}
+            <div className="modal fade" id="musicHelpWindow" tabIndex="-1" aria-labelledby="musicHelpWindowLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+
+                    <div className="modal-body">
+                        <p>
+                            Certain web browsers such as Google Chrome do not allow us to play background music automatically.
+                            They require people like you to use a visible audio player like the one we provided below the buttons. 
+                            Without such a player, you would not be able to hear the music at all.
+                        </p> 
+                        <button type="button" className="btn button" data-bs-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MainMenu;
